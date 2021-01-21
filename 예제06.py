f8141198ff8dea041791e27afe5067e822a478f9
eeezi <76932879+eeezi@users.noreply.github.com>
2021-01-21 20:56:00 +0900

    입력받은 숫자만큼 별 출력 +@

--- 예제06.py
+++ 예제06.py
@@ -3,3 +3,9 @@ n = list(map(int, input()))
 for i in range(len(n)):
     print("*" * n[i], end="")
     print("")
+
+
+for i in range(len(n)):
+    for k in range(int(n[i])):
+        print("*", end="")
+    print("")
